c4fc17852634fda2929f395e12ae4f67da07025c
ahmed <ahmedzaida464@gmail.com>
2024-06-11 18:11:56 +0300

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-  types: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
